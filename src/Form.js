050a68ad6a60fe8834c7fad9386366ebca0d8d2f
Tanmoy <tanmoysaha710@gmail.com>
2023-06-24 20:10:48 +0530

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters long");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    if (phoneNumber.length < 10) {
+      alert("Phone number should have at least 10 digits");
+      return;
+    }
+
+    setRegistrationSuccess(true);
+  };
+
+  return (
+    <div className="registration-form">
+      <h2>Registration Form</h2>
+      <p>Update Your Details</p>
+      {registrationSuccess ? (
+        <div className="success-message">
+          <p>Registration successful!</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Phone Number</label>
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit">Register</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Form;
